a6d5e26c0478a2160399a2bcd97d8ebc5f9dc5ea
jalili.developer@gmail.com <jalili.developer@gmail.com>
2018-02-01 11:29:53 -0800

    asd

--- PracticOne/app.js
+++ PracticOne/app.js
@@ -570,6 +570,47 @@ console.log(t.toString())
 
 
 
+function Line() {
+
+
+    // Uses AXEL's node module
+    var ctx = require('./Axel');
+
+    // Clear the terminal
+    ctx.clear();
+
+    // Sets the pixel BG color to green
+    ctx.bg(0, 255, 0);
+
+    // Draws a line into the console
+    ctx.line(1, 1, 10, 10);
+
+
+    ctx.cursor.restore();
+}
+
+
+function Point() {
+
+    // Uses AXEL's node module
+    var ctx = require('./Axel');
+
+    // Clear the terminal
+    ctx.clear();
+
+    // Sets the pixel BG color to orange
+    ctx.bg(0, 128, 255);
+
+    // Draw 100 random blue dots on the console
+    for (var i = 0; i < 100; i += 1) {
+        ctx.point(
+            Math.random() * ctx.cols,
+            Math.random() * ctx.rows
+        );
+    }
+
+    ctx.cursor.restore();
+}
 
 
 
